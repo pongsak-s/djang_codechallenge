07f223f27ecca0ea9491f48a063e0a5be304a8de
Pongsak Srithongnopawong <pongsaks@Pongsaks-MacBook-Air.local>
2021-10-24 23:57:40 +0700

    Reverse Heroku steps

--- src/django_main/django_main/settings.py
+++ src/django_main/django_main/settings.py
@@ -79,8 +79,7 @@ WSGI_APPLICATION = 'django_main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': os.environ['DBNAMEPREFIX']+os.environ['STATE'],
-        'NAME': os.environ['DBNAME'],
+        'NAME': os.environ['DBNAMEPREFIX']+os.environ['STATE'],
         'USER': os.environ['DBUSER'],
         'PASSWORD': os.environ['DBPASSWORD'],
         'HOST': os.environ['DBHOST'],
